eed55afa94012c4867ad4f9b64b02ebeea9316ae
Manar Al Mashi <manar.almashii@gmail.com>
2025-10-28 04:37:36 +0300

    add User urls, views, serializers

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-# from .views import 
+from .views import Home, UserListView, UserDetailView
 
 urlpatterns = [
-    # path('', , name=''),
+    path('', Home.as_view(), name=''), # for test
+    path('users/', UserListView.as_view(), name='list-all-users'),
+    path('users/<int:user_id>/', UserDetailView.as_view(), name='display-user-details'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+
+from .models import User
+from .serializers import UserSerializer
+
+# User = get_user_model()
 
 # Create your views here.
+
+class Home(APIView):
+    def get(self, request):
+        content = {'message': 'Welcome to the GoLeave API !'}
+        return Response(content)
+    
+class UserListView(APIView):
+    def get(self, request):
+        # Get all of users from the DB
+        queryset = User.objects.all()
+        
+        # convert to a JSON using a serializer
+        serializer = UserSerializer(queryset, many=True)
+
+        return Response(serializer.data)
+
+class UserDetailView(APIView):
+    def get(self, request, user_id):
+        try:
+            # Get single user from the DB using her id
+            queryset = get_object_or_404(User, id=user_id)
+
+            # convert to a JSON using a serializer
+            serializer = UserSerializer(queryset)
+
+            # return a Response
+            return Response(serializer.data)
+        
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
