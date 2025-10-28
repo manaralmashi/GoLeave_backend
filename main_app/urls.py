ceae8b0126022782e96da90987bc47b407aac80c
Manar Al Mashi <manar.almashii@gmail.com>
2025-10-28 05:13:21 +0300

    add Full CRUD for Employee model

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import Home, UserListView, UserDetailView
+from .views import Home, UserListView, UserDetailView, EmployeeListView, EmployeeCreateView, EmployeeDetailView, EmployeeUpdateView, EmployeeDeleteView
 
 urlpatterns = [
     path('', Home.as_view(), name=''), # for test
     path('users/', UserListView.as_view(), name='list-all-users'),
     path('users/<int:user_id>/', UserDetailView.as_view(), name='display-user-details'),
+    path('employees/', EmployeeListView.as_view(), name='list-all-employees'),
+    path('employees/new/', EmployeeCreateView.as_view(), name='create-employee'),
+    path('employees/<int:employee_id>/', EmployeeDetailView.as_view(), name='display-employee-details'),
+    path('employees/<int:employee_id>/edit/', EmployeeUpdateView.as_view(), name='update-employee'),
+    path('employees/<int:employee_id>/delete/', EmployeeDeleteView.as_view(), name='delete-employee'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -5,10 +5,8 @@ from rest_framework import status
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 
-from .models import User
-from .serializers import UserSerializer
-
-# User = get_user_model()
+from .models import User, Employee
+from .serializers import UserSerializer, EmployeeSerializer
 
 # Create your views here.
 
@@ -16,7 +14,8 @@ class Home(APIView):
     def get(self, request):
         content = {'message': 'Welcome to the GoLeave API !'}
         return Response(content)
-    
+
+
 class UserListView(APIView):
     def get(self, request):
         # Get all of users from the DB
@@ -26,6 +25,7 @@ class UserListView(APIView):
         serializer = UserSerializer(queryset, many=True)
 
         return Response(serializer.data)
+
 
 class UserDetailView(APIView):
     def get(self, request, user_id):
@@ -39,5 +39,79 @@ class UserDetailView(APIView):
             # return a Response
             return Response(serializer.data)
         
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+class EmployeeListView(APIView):
+    def get(self, request):
+        # Get all of all employees from the DB
+        queryset = Employee.objects.all()
+        
+        # convert to a JSON using a serializer
+        serializer = EmployeeSerializer(queryset, many=True)
+
+        return Response(serializer.data)
+
+
+class EmployeeCreateView(APIView):
+    def post(self, request):
+        try:
+            serializer = EmployeeSerializer(data=request.data)
+
+            if serializer.is_valid(): # if all fields is valid,
+                serializer.save() # save it to thr DB
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            # if any field is Invalid, return this
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class EmployeeDetailView(APIView):
+    def get(self, request, employee_id):
+        try:
+            # Get single Employee from the DB using her id
+            queryset = get_object_or_404(Employee, id=employee_id)
+
+            # convert to a JSON using a serializer
+            serializer = EmployeeSerializer(queryset)
+
+            # return a Response
+            return Response(serializer.data)
+        
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class EmployeeUpdateView(APIView):
+    def put(self, request, employee_id):
+        try:
+            # Get the single employee from the DB
+            # Look up an employee in the DB and if it does not exist return a 404 
+            queryset = get_object_or_404(Employee, id=employee_id)
+            # Overwrite it with the new data
+            serializer = EmployeeSerializer(queryset, data=request.data)
+            # save it!
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class EmployeeDeleteView(APIView):
+    def delete(self, request, employee_id):
+        try:
+            # Get an employee or return a 404
+            queryset = get_object_or_404(Employee, id=employee_id)
+            # delete the employee
+            queryset.delete()
+            # return a response
+            return Response({'message': f'Employee {employee_id} has been deleted!'}, status=status.HTTP_204_NO_CONTENT)
+
         except Exception as error:
             return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
