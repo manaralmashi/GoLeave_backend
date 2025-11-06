0f94601c0406d83b5c63ab5d063b4a37b58beb8c
Manar Al Mashi <manar.almashii@gmail.com>
2025-11-06 08:02:28 +0300

    update serializers, urls

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -4,27 +4,26 @@ from .models import User, Employee, LeaveType, LeaveRequest, LeaveBalance
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
-
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_active', 'date_joined']
 
 class EmployeeSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
     class Meta:
         model = Employee
-        fields = '__all__'
-
+        fields = ['id', 'user', 'job_title', 'department', 'role', 'hire_date']
 
 class LeaveTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = LeaveType
         fields = '__all__'
 
-
 class LeaveRequestSerializer(serializers.ModelSerializer):
+    employee_details = EmployeeSerializer(source='employee', read_only=True)
+    leave_type_details = LeaveTypeSerializer(source='leave_type', read_only=True)
     class Meta:
         model = LeaveRequest
         fields = '__all__'
-
-
+    
 class LeaveBalanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = LeaveBalance
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,20 +1,22 @@
 from django.urls import path
-from .views import Home, UserListView, UserDetailView, EmployeeListView, EmployeeCreateView, EmployeeDetailView, EmployeeUpdateView, EmployeeDeleteView, LeaveTypeListView, LeaveTypeUpdateView, LeaveRequestListView, LeaveRequestCreateView, LeaveRequestDetailView, LeaveRequestUpdateView, LeaveRequestDeleteView, ApproveLeaveRequestView, RejectLeaveRequestView, PendingLeaveRequestView, LeaveBalanceListView, LeaveBalanceByEmployeeView, SignupUserView
+from .views import Home, UserListView, UserDetailView, EmployeeListCreateView, EmployeeDetailView, EmployeeUpdateView, EmployeeDeleteView, LeaveTypeListView, LeaveTypeUpdateView, LeaveRequestListCreateView, LeaveRequestDetailView, LeaveRequestUpdateView, LeaveRequestDeleteView, ApproveLeaveRequestView, RejectLeaveRequestView, PendingLeaveRequestView, LeaveBalanceListView, LeaveBalanceByEmployeeView, SignupUserView, DashboardStatsView # EmployeeListView, EmployeeCreateView, LeaveRequestListView, LeaveRequestCreateView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('', Home.as_view(), name=''), # for test
     path('users/', UserListView.as_view(), name='list-all-users'),
     path('users/<int:user_id>/', UserDetailView.as_view(), name='display-user-details'),
-    path('employees/', EmployeeListView.as_view(), name='list-all-employees'),
-    path('employees/new/', EmployeeCreateView.as_view(), name='create-employee'),
+    # path('employees/', EmployeeListView.as_view(), name='list-all-employees'),
+    # path('employees/', EmployeeCreateView.as_view(), name='create-employee'),
+    path('employees/', EmployeeListCreateView.as_view(), name='employees'),
     path('employees/<int:employee_id>/', EmployeeDetailView.as_view(), name='display-employee-details'),
     path('employees/<int:employee_id>/edit/', EmployeeUpdateView.as_view(), name='update-employee'),
     path('employees/<int:employee_id>/delete/', EmployeeDeleteView.as_view(), name='delete-employee'),
     path('leave-types/', LeaveTypeListView.as_view(), name='list-all-leave-types'),
     path('leave-types/<int:leave_type_id>/edit/', LeaveTypeUpdateView.as_view(), name='list-all-leave-types'),
-    path('leave-requests/', LeaveRequestListView.as_view(), name='list-all-leave-requests'),
-    path('leave-requests/new/', LeaveRequestCreateView.as_view(), name='create-leave-request'),
+    # path('leave-requests/', LeaveRequestListView.as_view(), name='list-all-leave-requests'),
+    # path('leave-requests/', LeaveRequestCreateView.as_view(), name='create-leave-request'),
+    path('leave-requests/', LeaveRequestListCreateView.as_view(), name='leave-requests'),
     path('leave-requests/<int:leave_request_id>/', LeaveRequestDetailView.as_view(), name='display-leave-request-details'),
     path('leave-requests/<int:leave_request_id>/edit/', LeaveRequestUpdateView.as_view(), name='update-leave-request'),
     path('leave-requests/<int:leave_request_id>/delete/', LeaveRequestDeleteView.as_view(), name='delete-leave-request'),
@@ -27,4 +29,5 @@ urlpatterns = [
     path('signup/', SignupUserView.as_view(), name='signup'),
     path('login/', TokenObtainPairView.as_view(), name='login'), # token obtain pair
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # get new access token
+    path('dashboard/stats/', DashboardStatsView.as_view(), name='dashboard-stats'),
 ]
